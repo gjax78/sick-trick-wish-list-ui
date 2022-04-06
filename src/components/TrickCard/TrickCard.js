e1b06fd65ebbc51848dcbb38cd80036266358cc9
Geena Jackson <gjacks303@gmail.com>
2022-04-06 09:57:08 -0600

    Pass props to Trick Card

--- src/components/TrickCard/TrickCard.js
+++ src/components/TrickCard/TrickCard.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
-const TrickCard = () => {
+const TrickCard = ({ stance, name, obstacle, tutorial, id }) => {
   return (
-    <h1>Trick Card</h1>
+    <div className='trick-card'>
+      <h1>Trick Card</h1>
+      <h3>{name}</h3>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+    </div>
   )
 }
 
