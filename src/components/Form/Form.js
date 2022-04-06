eb68d4b6f06519497307680a924ee49de5d027d1
Geena Jackson <gjacks303@gmail.com>
2022-04-06 10:40:04 -0600

    Create basic form without select inputs

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -13,7 +13,38 @@ class Form extends Component {
 
   render() {
     return (
+      <form>
 
+        <input
+          type='text'
+          placeholder='stance'
+          name='stance'
+          value={this.state.stance}
+        />
+
+        <input
+          type='text'
+          placeholder='name'
+          name='name'
+          value={this.state.name}
+        />
+
+        <input
+          type='text'
+          placeholder='obstacle'
+          name='obstacle'
+          value={this.state.obstacle}
+        />
+
+        <input
+          type='text'
+          placeholder='tutorial'
+          name='tutorial'
+          value={this.state.tutorial}
+        />
+
+        <button>SEND IT</button>
+      </form>
     )
   }
 }
